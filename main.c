854e1609e2baf61c2b93c88e80773bfb2d568b58
Osfield Gaga <osfieldgaga@gmail.com>
2025-11-23 18:01:05 -0500

    added comment in keypad file

--- main.c
+++ main.c
@@ -12,11 +12,18 @@
 #include "keypad.h"
 #include <stdint.h>
 
+#define POT PORTAbits.RA3
+#define POT_TRIS TRISAbits.TRISA3
+#define POT_ANSEL ANSELAbits.ANSELA3
+
 int main(void) {
 
     init_T1();
     init_seven_seg();
     init_keypad();
+    
+     POT_TRIS = TRIS_INPUT;
+    POT_ANSEL = ANSEL_ANALOG;
 
     unsigned char c, key_in;
 
@@ -32,7 +39,13 @@ int main(void) {
             }
         }
 
-        print_seven_seg(c, c, c, c);
+//        print_seven_seg((POT & 0xF000) >> 12, (POT & 0xF00) >> 8, (POT & 0xF0) >> 4, POT & 0xF);
+        
+        if (POT > 0) {
+            print_seven_seg(1, 1, 1, 1);
+        } else {
+            print_seven_seg(5, 5, 2, 2);
+        }
 
     }
     return 0;
